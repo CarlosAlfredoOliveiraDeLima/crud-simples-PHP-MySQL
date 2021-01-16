04d7671f1b24939292bdff58d9715de6159430fe
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2021-01-16 20:01:36 -0300

    Criação do formulário de cadastro. Eliminação de arquivos desnecessários

--- index.php
+++ index.php
@@ -6,12 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
 
     <title>Cadastro</title>
 </head>
 <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Cadastro</h1>
+                <form action="cadastro_script.php" method="post">
+                    <div class="form-group">
+                        <label for="nome">Nome completo:</label>
+                        <input type="text" class="form-control" name="nome">
+                    </div>
+                    <div class="form-group">
+                        <label for="endereco">Endereço:</label>
+                        <input type="text" class="form-control" name="endereco">
+                    </div>
+                    <div class="form-group">
+                        <label for="telefone">Telefone:</label>
+                        <input type="text" class="form-control" name="telefone">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="endereco">Endereço:</label>
+                        <input type="email" class="form-control" name="endereco">
+                    </div>
+                    <div class="form-group">
+                        <label for="dt_nasc">Data de Nacimento:</label>
+                        <input type="date" class="form-control" name="dt_nasc">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-success">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
