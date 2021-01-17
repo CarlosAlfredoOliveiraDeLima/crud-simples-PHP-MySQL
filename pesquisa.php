0532d2937862d1abb564153f6ff5d41d7754bcc7
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2021-01-17 19:40:17 -0300

    Estrutura condicional de exclusão de cadastro de pessoa com envio dos dados para o script de exclusão

--- pesquisa.php
+++ pesquisa.php
@@ -65,8 +65,9 @@
                                         <td>$data_nascimento</td>
                                         <td width=150px>
                                             <a href='editar_cadastro.php?id=$cod_pessoa' class='btn btn-success btn-sm'>Editar</a>
-                                            <a href='' class='btn btn-danger btn-sm'>Excluir</a>
-                                        </td>
+                                            <a href='' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#modal_confirma' 
+                                            onclick=" .'"'."pegar_dados($cod_pessoa, '$nome')" .'"'.">Excluir</a>
+                                        </td>                                           
                                     </tr>
                                 ";
                             }
@@ -77,5 +78,43 @@
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="modal_confirma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Confirmar exclusão</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="excluir_script.php" method="post">
+                    <div class="modal-body">
+                        <p>Deseja realmente excluir <b id="nome_pessoa">Nome da pessoa</b>?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+                        <input type="hidden" name="id" id="cod_pessoa" value="">
+                        <input type="submit" class="btn btn-danger" value="Sim">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        function pegar_dados(id, nome){
+            document.getElementById('nome_pessoa').innerHTML = nome;
+            document.getElementById('cod_pessoa').value = id;
+        }
+    </script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 </html>
