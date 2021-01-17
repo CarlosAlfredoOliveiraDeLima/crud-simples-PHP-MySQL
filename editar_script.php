5252e03cb46f110c16020c2d0f33fcc2a1e10fd6
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2021-01-17 15:03:02 -0300

    Eliminando código js desnecessário

--- editar_script.php
+++ editar_script.php
@@ -11,41 +11,34 @@
     <title>Alteração de Cadastro</title>
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <?php
-            include 'conexao.php';
-            $id = $_POST['id'];
-            $nome = $_POST['nome'];
-            $endereco = $_POST['endereco'];
-            $telefone = $_POST['telefone'];
-            $email = $_POST['email'];
-            $dt_nasc = $_POST['dt_nasc'];
+    <div class="container">
+        <div class="row">
+            <?php
+                include 'conexao.php';
+                $id = $_POST['id'];
+                $nome = $_POST['nome'];
+                $endereco = $_POST['endereco'];
+                $telefone = $_POST['telefone'];
+                $email = $_POST['email'];
+                $dt_nasc = $_POST['dt_nasc'];
 
 
-            $sql = "update pessoas set nome = '$nome', endereco = '$endereco', telefone = '$telefone', email = '$email', data_nascimento = '$dt_nasc' where cod_pessoa = '$id'";
+                $sql = "update pessoas set nome = '$nome', endereco = '$endereco', telefone = '$telefone', email = '$email', data_nascimento = '$dt_nasc' where cod_pessoa = '$id'";
 
-            if (mysqli_query($conn, $sql)){
-                mensagem("$nome atualizado com sucesso!", "success");
-            }else {
-                mensagem("$nome não alterado!", "danger");
-            }
-        ?>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <a href="index.php" class="btn btn-primary">Voltar</a>
+                if (mysqli_query($conn, $sql)){
+                    mensagem("$nome atualizado com sucesso!", "success");
+                }else {
+                    mensagem("$nome não alterado!", "danger");
+                }
+            ?>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <a href="index.php" class="btn btn-primary">Voltar</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
