3b6a71ad13ce4fc81f1630c228883e050db1c955
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2021-01-17 11:54:21 -0300

    Refatoração do index.php, criação das páginas de cadastro e pesquisa de pessoas.

--- index.php
+++ index.php
@@ -8,47 +8,29 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.css">
 
-    <title>Cadastro</title>
+    <title>Empresa</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>Cadastro</h1>
-                <form action="cadastro_script.php" method="post">
-                    <div class="form-group">
-                        <label for="nome">Nome completo:</label>
-                        <input type="text" class="form-control" name="nome" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="endereco">Endereço:</label>
-                        <input type="text" class="form-control" name="endereco" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="telefone">Telefone:</label>
-                        <input type="text" class="form-control" name="telefone" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">E-mail:</label>
-                        <input type="email" class="form-control" name="email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="dt_nasc">Data de Nacimento:</label>
-                        <input type="date" class="form-control" name="dt_nasc" required>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-success">
-                    </div>
-                </form>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="jumbotron">
+                <h1 class="display-4">Cadastro Web</h1>
+                <p class="lead">Este é um sistema simplificado de cadastro. Base de estudos para criação de sistemas Web com PHP e MySQL.</p>
+                <hr class="my-4">
+                <p>Acesse as funções</p>
+                <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastro</a>
+                <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">Pesquisa</a>
             </div>
         </div>
     </div>
+</div>
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
